9a9362b60a5b3dbddd9d40b1f62db5de2d1e8d17
Raahim58 <raahimabdulsamad@gmail.com>
2025-01-06 23:42:37 +0500

    added pages

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import SignUpPage from './pages/SignUpPage';
+import LoginPage from './pages/LoginPage';
+
+export default function App() {
+    return (
+      <div>
+        <Routes>
+          <Route path = '/' element = {<HomePage/>} />
+          <Route path = '/signup' element = {<SignUpPage/>} />
+          <Route path = '/login' element = {<LoginPage/>} />
+        </Routes>
+      </div>
+    )
+}
