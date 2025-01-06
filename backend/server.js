0b0fc882367d3e560af85f11613788991611e167
Raahim58 <raahimabdulsamad@gmail.com>
2025-01-06 15:44:39 +0500

    creating product auth

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import authRoutes from './routes/auth.route.js';
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
+
+import productRoutes from './routes/product.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -11,7 +13,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // allows you to parse a body of JSON data 
 app.use(cookieParser());    
+
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
     console.log('Server is running on http://localhost:' + PORT);
