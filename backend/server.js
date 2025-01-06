5c065e2efc31c6a56859297a82a2d5bba4029fad
Raahim58 <raahimabdulsamad@gmail.com>
2025-01-06 19:01:16 +0500

    created coupon backend functionality

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 
+import { connectDB } from './lib/db.js';
 import productRoutes from './routes/product.route.js';
 import authRoutes from './routes/auth.route.js';
 import cartRoutes from './routes/cart.route.js';
+import couponRoutes from './routes/coupon.route.js';
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
 
 app.listen(PORT, () => {
     console.log('Server is running on http://localhost:' + PORT);
