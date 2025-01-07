ef8e875cb63d043bb37ee55827b6f9d755154478
Raahim58 <raahimabdulsamad@gmail.com>
2025-01-07 16:34:23 +0500

    added assets

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,53 @@
-const HomePage = () => {
-    return (
-        <h1> HomePage </h1>
-    )
-}
+import { useEffect } from "react";
+import CategoryItem from "../components/CategoryItem";
+import { useProductStore } from "../stores/useProductStore";
+import FeaturedProducts from "../components/FeaturedProducts";
 
-export default HomePage
+import jeansImg from '../assets/jeans.jpg';
+import tshirtsImg from '../assets/tshirts.jpg';
+import shoesImg from '../assets/shoes.jpg';
+import glassesImg from '../assets/glasses.png';
+import jacketsImg from '../assets/jackets.jpg';
+import suitsImg from '../assets/suits.jpg';
+import bagsImg from '../assets/bags.jpg';
+
+const categories = [
+  { href: "/jeans", name: "Jeans", imageUrl: jeansImg },
+  { href: "/t-shirts", name: "T-shirts", imageUrl: tshirtsImg },
+  { href: "/shoes", name: "Shoes", imageUrl: shoesImg },
+  { href: "/glasses", name: "Glasses", imageUrl: glassesImg },
+  { href: "/jackets", name: "Jackets", imageUrl: jacketsImg },
+  { href: "/suits", name: "Suits", imageUrl: suitsImg },
+  { href: "/bags", name: "Bags", imageUrl: bagsImg },
+];
+
+
+const HomePage = () => {
+	const { fetchFeaturedProducts, products, isLoading } = useProductStore();
+
+	useEffect(() => {
+		fetchFeaturedProducts();
+	}, [fetchFeaturedProducts]);
+
+	return (
+		<div className='relative min-h-screen text-white overflow-hidden'>
+			<div className='relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16'>
+				<h1 className='text-center text-5xl sm:text-6xl font-bold text-emerald-400 mb-4'>
+					Explore Our Categories
+				</h1>
+				<p className='text-center text-xl text-gray-300 mb-12'>
+					Discover the latest trends in eco-friendly fashion
+				</p>
+
+				<div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
+					{categories.map((category) => (
+						<CategoryItem category={category} key={category.name} />
+					))}
+				</div>
+
+				{!isLoading && products.length > 0 && <FeaturedProducts featuredProducts={products} />}
+			</div>
+		</div>
+	);
+};
+export default HomePage;
