fdb47114600b5ed3b18069077d87e4cfb4d5a794
Raahim58 <raahimabdulsamad@gmail.com>
2025-01-08 01:22:09 +0500

    fixing order summary dependencies

--- frontend/src/components/OrderSummary.jsx
+++ frontend/src/components/OrderSummary.jsx
@@ -6,7 +6,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import axios from "../lib/axios";
 
 const stripePromise = loadStripe(
-	"pk_test_51KZYccCoOZF2UhtOwdXQl3vcizup20zqKqT9hVUIsVzsdBrhqbUI2fE0ZdEVLdZfeHjeyFXtqaNsyCJCmZWnjNZa00PzMAjlcL"
+	"pk_test_51QeH3FDbbvZzb2vEWr4LywhU51yu1zs8XZ1g7L0SuD1gXEvpBQgxJRHXhGl6y7E0ojGnLzNPnzkLJ1ViiUbyrmud00kD25JOS4"
 );
 
 const OrderSummary = () => {
