2308cd5b999c60d7ed61a1070ccda1991b1c1278
Raahim58 <raahimabdulsamad@gmail.com>
2025-01-08 18:58:22 +0500

    deployment pt.4 and a few tweaks

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -21,33 +21,38 @@ const categories = [
   { href: "/bags", name: "Bags", imageUrl: bagsImg },
 ];
 
-
 const HomePage = () => {
-	const { fetchFeaturedProducts, products, isLoading } = useProductStore();
+  const { fetchFeaturedProducts, products, isLoading } = useProductStore();
 
-	useEffect(() => {
-		fetchFeaturedProducts();
-	}, [fetchFeaturedProducts]);
+  useEffect(() => {
+    fetchFeaturedProducts();
+  }, [fetchFeaturedProducts]);
 
-	return (
-		<div className='relative min-h-screen text-white overflow-hidden'>
-			<div className='relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16'>
-				<h1 className='text-center text-5xl sm:text-6xl font-bold text-emerald-400 mb-4'>
-					explore our categories
-				</h1>
-				<p className='text-center text-xl text-gray-300 mb-12'>
-					discover the latest trends in eco-friendly fashion
-				</p>
+  return (
+    <div className='relative min-h-screen text-white overflow-hidden'>
+      <div className='relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16'>
+        <h1 className='text-center text-5xl sm:text-6xl font-bold text-emerald-400 mb-4'>
+          explore our categories
+        </h1>
+        <p className='text-center text-xl text-gray-300 mb-12'>
+          discover the latest trends in eco-friendly fashion
+        </p>
 
-				<div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
-					{categories.map((category) => (
-						<CategoryItem category={category} key={category.name} />
-					))}
-				</div>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 text-center'>
+          {categories.map((category, index) => (
+            <div 
+              key={category.name} 
+              className={`${index === categories.length - 1 ? "lg:col-span-3 lg:col-start" : ""}`}
+            >
+              <CategoryItem category={category} />
+            </div>
+          ))}
+        </div>
 
-				{!isLoading && products.length > 0 && <FeaturedProducts featuredProducts={products} />}
-			</div>
-		</div>
-	);
+        {!isLoading && products.length > 0 && <FeaturedProducts featuredProducts={products} />}
+      </div>
+    </div>
+  );
 };
+
 export default HomePage;
